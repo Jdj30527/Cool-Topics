084089834b9c88928fa003a09a3b443847e5fd67
John Jefferies <johnj17@vt.edu>
2018-04-22 10:35:55 -0400

    New changes

--- Cool Topics Project.playground/Contents.swift
+++ Cool Topics Project.playground/Contents.swift
@@ -5,10 +5,22 @@ import UIKit
 var str = "Hello, playground"
 
 print(str)
+
 func printNum() {
     for _ in 1...6 {
         print(str)
     }
 }
 
-printNum()
+//printNum()
+
+
+var answer = 1
+func nextOne() {
+    for _ in 1...6 {
+        print(answer * 2)
+        answer = answer * 2
+    }
+}
+
+nextOne()
