dc5159262a41a78055c7d4e4fc6b27478efc7122
John Jefferies <johnj17@vt.edu>
2018-04-22 10:13:36 -0400

    First commit

---
+++ Cool Topics Project.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+func printNum() {
+    for _ in 1...6 {
+        print(str)
+    }
+}
+
+printNum()
